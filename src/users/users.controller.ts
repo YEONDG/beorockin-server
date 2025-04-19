0337ce7b3f54af233055a13e4c1676dd1db46666
YDG <zzmn1234@naver.com>
2025-04-19 15:08:23 +0900

    feat: user update authorization check

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,11 +7,14 @@ import {
   Put,
   Body,
   Delete,
+  ForbiddenException,
+  Req,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { ApiTags, ApiCookieAuth, ApiOperation } from '@nestjs/swagger';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserData } from 'src/auth/jwt.strategy';
 
 @ApiTags('users')
 @Controller('user')
@@ -41,7 +44,20 @@ export class UserController {
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UpdateUserDto,
+    @Req() req: Request & { user: UserData },
   ) {
+    // 현재 로그인한 사용자의 ID
+    const currentUserId = parseInt(req.user.userId);
+
+    // 자신의 정보를 수정하는 경우 또는 관리자인 경우만 허용
+    if (
+      currentUserId !== id
+      //  && req.user.role !== 'admin'
+    ) {
+      throw new ForbiddenException(
+        '다른 사용자의 정보를 수정할 권한이 없습니다.',
+      );
+    }
     return this.usersService.update(id, updateUserDto);
   }
 
