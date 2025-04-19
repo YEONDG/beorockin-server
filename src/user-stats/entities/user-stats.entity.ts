e6fb4afc5403483c5711d5d2a61d365b98bdc12d
YDG <zzmn1234@naver.com>
2025-04-19 15:08:30 +0900

    feat: add UserStats entity for tracking user statistics

---
+++ src/user-stats/entities/user-stats.entity.ts
@@ -0,0 +1,30 @@
+import { User } from 'src/users/entities/user.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+
+@Entity('user_stats')
+export class UserStats {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @OneToOne(() => User)
+  @JoinColumn()
+  user: User;
+
+  @Column({ default: 0 })
+  streakDays: number;
+
+  @Column({ default: 0 })
+  completedQuizzes: number;
+
+  @Column({ name: 'last_study_date', type: 'timestamp', nullable: true })
+  lastStudyDate: Date | null;
+
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
